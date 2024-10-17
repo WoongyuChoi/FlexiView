3d41be9a4c5be1f6dd49340cf74d5e351551e9ae
Schna <schna@kakao.com>
2024-10-17 15:34:16 +0900

    feature : ThemeProvider 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ThemeProvider, createTheme } from "@mui/material";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#ff4081",
+    },
+  },
+  typography: {
+    fontFamily: "Prestendard, Arial, sans-serif",
+    allVariants: {
+      fontFamily: "Prestendard, Arial, sans-serif",
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
